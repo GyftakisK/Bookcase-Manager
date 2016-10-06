85dc8c8f37cfe599459cb19b4dc2a52ba448158c
kgyftaki <konstantinos.gyftakis@nsn.com>
2016-10-06 12:46:42 +0300

    docstrings for bookcase_excel.py

--- bookcase_excel/bookcase_excel.py
+++ bookcase_excel/bookcase_excel.py
@@ -7,11 +7,20 @@ from bookcase_translations import Translations
 
 
 class Excel(object):
+    """
+    Class for input/output to Excel worksheet
+    """
     def __init__(self, path=lib.FileManager().path):
         self.path = path
 
     @staticmethod
     def validate_filename(filename):
+        """
+        Make sure that filename is consistent with the format of Bookcase manager
+        generated Excel files
+        :param filename:
+        :return: name of the database exported
+        """
         exp = re.compile(r'(?<=bookcase_).*(?=\.xlsx)')
         name = re.search(exp, filename)
         if not name:
@@ -19,6 +28,11 @@ class Excel(object):
         return name
 
     def read_excel(self, filename):
+        """
+        Reads the table from the specified Excel file
+        :param filename:
+        :return: tuple of tuples representing the table
+        """
         self.validate_filename(filename)
         workbook = xl.load_workbook(self.path + filename)
         result = []
@@ -26,10 +40,16 @@ class Excel(object):
             result.append(tuple(cell.value for cell in row))
         return tuple(result)
 
-    def write_excel(self, name, array):
+    def write_excel(self, name, table):
+        """
+        Writes a table to an excel file
+        :param name: the name of the exported DB
+        :param table: 2-D array containing DB table dump
+        :return: absolute path to file + filename
+        """
         workbook = xl.Workbook()
         worksheet = workbook.active
-        for row_num, row in enumerate(array):
+        for row_num, row in enumerate(table):
             for col_num, value in enumerate(row):
                 worksheet.cell(row=row_num + 1, column=col_num + 1, value=value)
         file = self.path + "bookcase_{name}.xlsx".format(name=name)
